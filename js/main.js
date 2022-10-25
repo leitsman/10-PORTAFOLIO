53840c7eba9bc72f089a903440138e3766427a18
leopori <100742075+leopori@users.noreply.github.com>
2022-10-25 14:18:24 -0500

    Cambios del CSS
    Cambios para no tener el problema del Navegador

--- js/main.js
+++ js/main.js
@@ -12,3 +12,21 @@ window.addEventListener('scroll', event=>{
         headerFix.classList.remove('header--fixed')
     }
 })
+
+
+('.navbar-nav div ul li a').click(function(e){
+    // Evitamos que se haga el scroll
+    e.preventDefault();
+
+    // Capturamos el ancla
+    let ancla = $(this).attr('href');
+
+    // Le quitamos el numeral # para solo quedarnos con el nombre de la sección
+    ancla = ancla.substring(1);
+
+    // Obtenemos la posición de la sección
+    let position = $('#'+ ancla).position();
+
+    // Hacemos el efecto scroll y le restamos algunos pixeles, en este caso 180
+    $('html, body').animate({scrollTop: (position.top - 600)}, 0);
+})
