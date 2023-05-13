9efc35f845a20dab15ae46281033b1487e6f511e
Emmanuel C <misterium273@gmail.com>
2023-05-12 23:57:20 -0400

    perf: es ahora una pagina totalmente responsiva solo falta añadir los portafolio

--- js/components/menu.js
+++ js/components/menu.js
@@ -1,21 +1,21 @@
 export function menuOpenClose() {
     const menuPrincipal = document.getElementById('menu-principal')
 
-    const imgOpenMenu = document.getElementById('menu--img-open')
-    const imgCloseMenu = document.getElementById('menu--img-close')
+    const imgOpenMenu = document.getElementById('menu__img-open')
+    const imgCloseMenu = document.getElementById('menu__img-close')
 
-    imgOpenMenu.addEventListener('click', event=> menuPrincipal.classList.add('menu--open'))
-    imgCloseMenu.addEventListener('click', event => menuPrincipal.classList.remove('menu--open'))
+    imgOpenMenu.addEventListener('click', event => menuPrincipal.classList.add('menu__open'))
+    imgCloseMenu.addEventListener('click', event => menuPrincipal.classList.remove('menu__open'))
     clickAndCloseMenu(menuPrincipal)
 }
 //! =============== CLICK AND CLOSE MENU
 function clickAndCloseMenu(elementHtml) {
-    const itemMenu = document.querySelectorAll('a.ul--item')
+    const itemMenu = document.querySelectorAll('a.ul__item')
 
-    itemMenu.forEach(button =>{
-    button.addEventListener('click', event=>{
-        elementHtml.classList.remove('menu--open')
-        
+    itemMenu.forEach(button => {
+        button.addEventListener('click', e => {
+            elementHtml.classList.remove('menu__open')
+
+        })
     })
-})
 }
