d32c7b802273fad2e5dc14ee04bdbb5504b44bb8
Emmanuel C <misterium273@gmail.com>
2023-06-30 14:48:47 -0400

    refactor: cambios en seccion portafolio y de color se cambio el color de la pagina se añadieron mas elementos a la seccion portafolio falta hacer responsivo la seccion portafolio

--- js/main.js
+++ js/main.js
@@ -2,49 +2,34 @@ import { menuOpenClose } from "./components/menu.js"
 
 window.addEventListener('DOMContentLoaded', e => {
     menuOpenClose()
-    // const redesIcon = document.querySelectorAll('.redes__icon')
-    // // console.log(redesIcon);
-    // if (screen.width >= 1024) {
-    //     // redesIcon.classList.add('fa-4x');
-    //     console.log(redesIcon);
-    // } else {
-    //     redesIcon.classList.remove('fa-4x')
-    // }
 })
 //! ================ HEADER-fixed ===============
 const headerFix = document.querySelector('.header')
 const headerContainer = document.querySelector('.header__container')
 const arrowUp = document.querySelector('.container__arrow-up')
-// console.log(arrowUp);
 window.addEventListener('scroll', e => {
-    if (window.scrollY >= 90) {
+    if (window.scrollY >= 1) {
         headerFix.classList.add('header__fixed')
         headerContainer.classList.add('header__container--shadow')
     } else {
         headerFix.classList.remove('header__fixed')
         headerContainer.classList.remove('header__container--shadow')
     }
-    if (window.scrollY >= 380) {
-        arrowUp.classList.add('container__arrow-up--active');
-    } else {
-        arrowUp.classList.remove('container__arrow-up--active')
-    }
 });
 //! ============== INCREMENT ICON ==============
 const redesIcon = document.querySelectorAll('.redes__icon')
-// console.log(redesIcon);
 window.addEventListener('resize', e => {
     const windowWidth = window.innerWidth
     if (windowWidth > 1024) {
         redesIcon.forEach(e => {
             e.classList.add('fa-4x');
             e.classList.remove('fa-3x');
-            console.log(e.classList);
+            // console.log(e.classList);
         });
     } else {
         redesIcon.forEach(e => {
             // e.classList.remove('fa-4x');
-            console.log(e.classList);
+            // console.log(e.classList);
         })
     }
 });
